77c9ceece1feaac276c743089a0ecc8fcfb0127a
Alan <alankerry468@gmail.com>
2023-12-26 19:21:55 +0300

    Добавление cors

--- server/app.js
+++ server/app.js
@@ -3,12 +3,14 @@ import user_mes_router from "./routers/user_mes_router.js"
 import orders_router from "./routers/orders_router.js"
 import { config } from "dotenv"
 import sequelize from "./db.js"
+import cors from "cors"
 
 config()
 
 const app = express()
 const PORT = process.env.PORT || 5000
 
+app.use(cors())
 app.use(express.json())
 app.use("/online_store/user_mes_api", user_mes_router)
 app.use("/online_store/orders_api", orders_router)
