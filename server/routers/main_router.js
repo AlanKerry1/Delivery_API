f58be65e3c01d9f38dbb9f45de213da8f412b35f
Alan <alankerry468@gmail.com>
2024-01-11 01:12:19 +0300

    Добавление пользователей - регистрация, авторизация, получение всех пользователей. Нет ролевой логики

--- server/routers/main_router.js
+++ server/routers/main_router.js
@@ -1,10 +1,12 @@
 import { Router } from "express"
 import messagesRouter from "./messages_router.js"
 import ordersRouter from "./orders_router.js"
+import userRouter from "./users_router.js"
 
 const router = new Router()
 
 router.use("/messages", messagesRouter)
 router.use("/orders", ordersRouter)
+router.use("/users", userRouter)
 
 export default router
